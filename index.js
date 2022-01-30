c2038b206eecf38866e016b7c83fc147a5fc0711
RutujaRM <96306705+RutujaRM@users.noreply.github.com>
2022-01-30 07:03:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+    var a=0, b=1, c=0, i;               //declaration of the variables
+
+    var n=prompt("Enter the number");  // here we get input from the user
+     n=parseInt(n)                     // for integer data type
+
+   for( i=1; i<=n; i++)
+     {
+               console.log(c)
+ 	        c=a+b;
+  		a=b;
+  		b=c;
+  		
+      }
+
+
+
+
+
